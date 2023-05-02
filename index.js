4f733e8791d05f4e12595097a4999a511ab9185d
NoahSantos <nsanto591@west-mec.org>
2023-05-01 17:51:02 -0700

    Minor changes
    Added descriptions to the projects and started editing the jquery.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // use dropshadow for the spotlight
+$(function(){
+    $(".next").click(function(){
+        // $(".active").css("transform", "scale(0.8)").removeClass("active");
+        // for(let i = 1; i < $(".carouselProjects").children(); i++){
+        //     $(".carouselProjects").eq(i).css("transform", "scale(1.2)").addClass("active").delay(500).css("transform", "scale(1)");
+        // }
+    })
+})
