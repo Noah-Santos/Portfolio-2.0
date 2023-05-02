a87a23917b7d616f437211d7073d6b0ef6d42c82
NoahSantos <nsanto591@west-mec.org>
2023-05-01 18:22:17 -0700

    Slides
    Trying to add shrinking and expanding transition for the slideshow.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-// use dropshadow for the spotlight
+// use box-shadow for the spotlight
 $(function(){
     $(".next").click(function(){
-        // $(".active").css("transform", "scale(0.8)").removeClass("active");
+        $(".active").css("transform", "scale(0.8)");
+        $(".active").next().css("transform", "scale(1.2)").addClass("active").css("transform", "scale(1)")
         // for(let i = 1; i < $(".carouselProjects").children(); i++){
+        //     if(i == $(".carouselProjects").children()-1){
+        //         i = 0;
+        //     }
+
         //     $(".carouselProjects").eq(i).css("transform", "scale(1.2)").addClass("active").delay(500).css("transform", "scale(1)");
+
         // }
     })
 })
