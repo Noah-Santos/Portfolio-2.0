f0ce02772df1a883e801a2669712c264b8b0e974
Noah-Santos <nsanto591@west-mec.org>
2023-05-07 11:43:33 -0700

    Blur
    Added blur effect to the home screen when it is hovered on.

--- js/index.js
+++ js/index.js
@@ -4,6 +4,16 @@ $(function(){
         let page = $(this).attr("id");
         window.location.href = `${page}.html`;
     })
+
+    // blurs other screens when one is hovered
+    $(".homeLinks").mouseenter(function(){
+        $(".homeScreens").not(this).css({filter:"blur(3px)"});
+    });
+
+    // unblurs when not hovering over screen
+    $(".homeLinks").mouseleave(function(){
+        $(".homeScreens").css({filter:"blur(0px)"});
+    })
 })
 
 
