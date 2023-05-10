00c9048562c72c524c0166584957f39646eac7ae
Noah-Santos <nsanto591@west-mec.org>
2023-05-09 22:13:08 -0700

    Nav
    Created the navbar and made the links function with the door animation.

--- js/door.js
+++ js/door.js
@@ -5,9 +5,28 @@ $(function(){
         // // closes door and displays new section then opens door
         $(".door").css({transform: "translateY(0%)"}).delay(2000).queue(function(){
             // removes active class from home page and adds to now active page
-            $(".homeGrid").removeClass("active").addClass("deactive");
+            $(".contentPage").removeClass("active").addClass("deactive");
             $(`.${page}`).removeClass("deactive").addClass("active");
             $(".door").css({transform: "translateY(-100%)"});
+            // clears queue
+            $(".door").clearQueue();
         });
+        
     })
+
+    // moves door and changes the page for navbar clicked
+    $(".navLink").click(function(){
+        // gets page id and turns it into the class form
+        let pg = $(this).attr("id");
+        pg = `${pg}Cont`;
+        $(".door").css({transform: "translateY(0%)"}).delay(2000).queue(function(){
+            // removes active class from home page and adds to now active page
+            $(".contentPage").removeClass("active").addClass("deactive");
+            $(`.${pg}`).removeClass("deactive").addClass("active");
+            $(".door").css({transform: "translateY(-100%)"});
+            // clears queue
+            $(".door").clearQueue();
+        });
+        
+    });
 })
