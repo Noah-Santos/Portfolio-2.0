150a9717c804f1fbf4e700805a6d743bc6a19fa8
Noah-Santos <nsanto591@west-mec.org>
2023-05-12 23:02:20 -0700

    Contact
    Got the contact page to somewhat work.

--- js/contact.js
+++ js/contact.js
@@ -1,8 +1,19 @@
 $(function(){
-    // on hover of the grid section, the content will show
-    let id;
-    $(".contactInfo").hover(function(){
+    // on click of the grid section, the content will show
+    $(".icon").click(function(){
         id = $(this).attr("id");
-        $(`#${id}Card`).css("display", "flex");
-    });
+        // hides the svg
+        $(this).fadeOut(500).queue(function(){
+            // makes content appear
+            $(`#${id}Info`).removeClass("deactive").addClass("active");
+            // after 2 seconds, info dissapears
+            // setTimeout(function(){
+            //     // hides info and displays svg
+            //     $(`#${id}Info`).removeClass("active").addClass("deactive");
+            //     $(`#${id}`).fadeIn(500);
+            // }, 2000)
+            // clears queue
+            $(".door").clearQueue();
+        });
+    })
 })
